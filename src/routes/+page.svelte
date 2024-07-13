f7db1f412e0342e1be0ecc3be1c6c5a1d2638f71
Sapna Dahikamble <140054005+863770@users.noreply.github.com>
2024-07-13 23:35:50 +0530

    Updated +page.svelte
    add code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,3 +36,23 @@
     </div>
     <button type="submit" class="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">Share</button>
 </form>
+import { redirect } from '@sveltejs/kit';
+import prisma from '$lib/prisma';
+export const actions = {
+    default: async ({ request }) => {
+        const data = Object.fromEntries(await request.formData());
+        let username = data.username;
+        let content = data.content;
+        let image = Buffer.from(await data.image.arrayBuffer()).toString('base64');
+        
+        await prisma.post.create({
+            data: {
+                username,
+                content,
+                image} });
+        throw redirect(302, '/');}
+    import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+export default prisma;
+
+
